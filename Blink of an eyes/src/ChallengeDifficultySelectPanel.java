2c0f3c88f9144052389b137cf681d05726ec97d9
Kim Jaeyong <jy3479@naver.com>
2021-11-28 21:11:49 +0900

    release 1.0.0

---
+++ Blink of an eyes/src/ChallengeDifficultySelectPanel.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ChallengeDifficultySelectPanel extends JPanel {
+    JLabel title;
+    JButton easyButton;
+    JButton normalButton;
+    JButton hardButton;
+    public ChallengeDifficultySelectPanel(){
+        this.setSize(1080,720);
+        this.setBackground(Color.BLACK);
+        this.setLayout(new FlowLayout(FlowLayout.CENTER,5,30));
+        Dimension button_size = new Dimension(720,120);
+        title = new JLabel("챌린지 모드");
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("한초롬바탕",Font.BOLD,70));
+        title.setPreferredSize(new Dimension(960, 80));
+        title.setHorizontalAlignment(JLabel.CENTER);
+        easyButton = new JButton("E A S Y");
+        easyButton.setBackground(Color.GRAY);
+        easyButton.setPreferredSize(button_size);
+        easyButton.setForeground(Color.BLACK);
+        easyButton.setFont(new Font("한초롬바탕",Font.PLAIN,50));
+        normalButton = new JButton("N O R M A L");
+        normalButton.setBackground(Color.GRAY);
+        normalButton.setPreferredSize(button_size);
+        normalButton.setForeground(Color.BLACK);
+        normalButton.setFont(new Font("한초롬바탕",Font.PLAIN,50));
+        hardButton = new JButton("H A R D");
+        hardButton.setBackground(Color.GRAY);
+        hardButton.setPreferredSize(button_size);
+        hardButton.setForeground(Color.BLACK);
+        hardButton.setFont(new Font("한초롬바탕",Font.PLAIN,50));
+        this.add(title);
+        this.add(easyButton);
+        this.add(normalButton);
+        this.add(hardButton);
+    }
+}
--- Blink of an eyes/src/MainFrame.java
+++ Blink of an eyes/src/MainFrame.java
@@ -4,10 +4,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
+    private int now = 0;
+    private int index = 0;
     MainMenuPanel mainMenuPanel = new MainMenuPanel();
     GameSelectPanel modeSelectPanel = new GameSelectPanel();
     TimeAttackModePlayPanel timeAttackModePlayPanel = new TimeAttackModePlayPanel("easy");
     TimeAttackDifficultySelectPanel timeAttackDifficultySelectPanel = new TimeAttackDifficultySelectPanel();
+    Challengemode challengemode = new Challengemode("easy");
+    ChallengeDifficultySelectPanel challengeDifficultySelectPanel = new ChallengeDifficultySelectPanel();
+    StageSelectPannel stageSelectPannel = new StageSelectPannel(now);
+    StoryMode storyMode;
+    TutorialMode TutorialMode = new TutorialMode();
     public MainFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
@@ -17,9 +24,19 @@ public class MainFrame extends JFrame {
         changePanel(mainMenuPanel);
         mainMenuPanel.gamePlay.addActionListener(new ScreenChangeActionListener(modeSelectPanel));
         modeSelectPanel.timeAttackButton.addActionListener(new ScreenChangeActionListener(timeAttackDifficultySelectPanel));
+        modeSelectPanel.challengeButton.addActionListener(new ScreenChangeActionListener(challengeDifficultySelectPanel));
+        modeSelectPanel.storyButton.addActionListener(new ScreenChangeActionListener(stageSelectPannel));
+        modeSelectPanel.tutorialButton.addActionListener(new TutorialModeActionListener(TutorialMode));
         timeAttackDifficultySelectPanel.easyButton.addActionListener(new TimeAttackModeStartActionListener("easy"));
         timeAttackDifficultySelectPanel.normalButton.addActionListener(new TimeAttackModeStartActionListener("normal"));
         timeAttackDifficultySelectPanel.hardButton.addActionListener(new TimeAttackModeStartActionListener("hard"));
+        challengeDifficultySelectPanel.easyButton.addActionListener(new ChllengeModeStartActionListener("easy"));
+        challengeDifficultySelectPanel.normalButton.addActionListener(new ChllengeModeStartActionListener("normal"));
+        challengeDifficultySelectPanel.hardButton.addActionListener(new ChllengeModeStartActionListener("hard"));
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                stageSelectPannel.stages[now][i][j].addActionListener(new StoryModeStartActionListener(now, i * 3 + j));
+            }
     }
     void changePanel(Container container){
         this.setContentPane(container);
@@ -44,6 +61,84 @@ public class MainFrame extends JFrame {
             timeAttackModePlayPanel = new TimeAttackModePlayPanel(difficulty);
             changePanel(timeAttackModePlayPanel);
             timeAttackModePlayPanel.gameOverPanel.mainMenuButton.addActionListener(new ScreenChangeActionListener(mainMenuPanel));
+        }
+    }
+
+    private class ChllengeModeStartActionListener implements ActionListener{
+        String difficulty;
+        public ChllengeModeStartActionListener(String difficulty){
+            this.difficulty = difficulty;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            challengemode = new Challengemode(difficulty);
+            changePanel(challengemode);
+            challengemode.gameOverPanel.mainMenuButton.addActionListener(new ScreenChangeActionListener(mainMenuPanel));
+        }
+    }
+    private class StoryModeStartActionListener implements ActionListener {
+        int now, i;
+
+        public StoryModeStartActionListener(int now, int j) {
+            this.now = now;
+            this.i = j;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            storyMode = new StoryMode(now + 1, i + 1);
+            changePanel(storyMode);
+            storyMode.gameWinPannel.stageButton.addActionListener(new ScreenChangeActionListener(stageSelectPannel));
+            storyMode.gameWinPannel.mainMenuButton.addActionListener(new ScreenChangeActionListener(mainMenuPanel));
+            storyMode.gameOverPannel.mainMenuButton.addActionListener(new ScreenChangeActionListener(mainMenuPanel));
+        }
+    }
+    private class StoryStagePrevActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (now > 0)
+                now--;
+            else
+                now = 0;
+            stageSelectPannel = new StageSelectPannel(now);
+            changePanel(stageSelectPannel);
+            stageSelectPannel.prev.addActionListener(new StoryStagePrevActionListener());
+            stageSelectPannel.next.addActionListener(new StoryStageNextActionListener());
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    stageSelectPannel.stages[now][i][j].addActionListener(new StoryModeStartActionListener(now, i * 3 + j));
+                }
+            }
+        }
+    }
+    private class StoryStageNextActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (now < 4)
+                now++;
+            else
+                now = 4;
+            stageSelectPannel = new StageSelectPannel(now);
+            changePanel(stageSelectPannel);
+            stageSelectPannel.next.addActionListener(new StoryStageNextActionListener());
+            stageSelectPannel.prev.addActionListener(new StoryStagePrevActionListener());
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    stageSelectPannel.stages[now][i][j].addActionListener(new StoryModeStartActionListener(now, i * 3 + j));
+                }
+            }
+        }
+    }
+    private  class TutorialModeActionListener implements  ActionListener{
+        public TutorialModeActionListener(TutorialMode tutorialMode) {
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e){
+            TutorialMode = new TutorialMode();
+            changePanel(TutorialMode);
+            TutorialMode.MainMenuGo.mainMenuButton.addActionListener(new ScreenChangeActionListener(mainMenuPanel));
+
         }
     }
 }
